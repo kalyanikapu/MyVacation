a5ca546a703397830bc5351b31be72c0b95bf18d
Sai Kapu <saikapu@Sais-MacBook-Air-2.local>
2020-04-01 06:46:38 -0700

    Adding MV project

---
+++ src/com/mv/airreservation/Passenger.java
@@ -0,0 +1,55 @@
+package com.mv.airreservation;
+
+import java.util.*;
+
+public class Passenger {
+	
+	private String passengerName;
+	private String classPref;
+	private String groupMembers;
+	
+	/**
+	 * A constructor to make the passenger
+	 * @param name - name of the passenger
+	 * @param classPreference - First class or economy
+	 * @param group - Individual or group name
+	 */
+	public Passenger(String name, String classPreference, String group) {
+		
+		this.passengerName = name;
+		this.classPref = classPreference;
+		this.groupMembers = group;
+		
+	}
+	
+	/**
+	 * Getter method for the passenger's name
+	 * @return passengerName - the name of the passenger
+	 */
+	public String getPassengerName() {
+		
+		return passengerName;
+		
+	}
+	
+	/**
+	 * Getter method to get the passenger's class preference
+	 * @return classPref - the class preference
+	 */
+	public String getClassPref() {
+		
+		return classPref;
+		
+	}
+	
+	/**
+	 * Getter method for the group
+	 * @return groupMembers - the passenger's group (null if no group)
+	 */
+	public String getGroup() {
+		
+		return groupMembers;
+		
+	}
+	
+}
